b1263034f0d989f45bc3573509c96bfb7eaa4810
QuanProCt1604 <127952803+QuanProCt1604@users.noreply.github.com>
2023-03-17 21:39:23 +0700

    Add files via upload

---
+++ NguyenHongQuan/IStudent.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Exam
+{
+    internal interface IStudent
+    {
+        int StudID
+        {
+            get;
+            set;
+        }
+        string StudName
+        {
+            get;
+            set;
+        }
+        string StudGender
+        {
+            get;
+            set;
+        }
+        int StudAge
+        {
+            get;
+            set;
+        }
+        string StudClass
+        {
+            get;
+            set;
+        }
+        float StudAvgMark
+        {
+            get;
+        }
+        void Print();
+
+    }
+}
+
---
+++ NguyenHongQuan/Student.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Exam
+{
+    class Student : IStudent
+    {
+        public Student()
+        {
+        }
+
+        public int StudID { get ; set; }
+        public string StudName { get; set ; }
+        public string StudGender { get; set; }
+        public int StudAge { get; set; }
+        public string StudClass { get; set; }
+        public float StudAvgMark { get;  }
+
+        public void Print()
+        {
+            Console.WriteLine("Student ID :"+this.StudID);
+            Console.WriteLine("Student Name : " +this.StudName);
+            Console.WriteLine("Student Gender : " + this.StudGender);
+            Console.WriteLine("Student Age : " + this.StudAge);
+            Console.WriteLine(" Student Class : " + this.StudClass);
+            Console.WriteLine(" Student Avg Mark " + this.StudAvgMark);
+
+        }
+
+        int[] MarkList;
+         int[] MarkList = new int[3];
+        public MarkList this[int index]
+        {
+
+        }
+
+    }
+}
+
