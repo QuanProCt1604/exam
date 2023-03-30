3c074f520f48d363c36d2f24134976257d10e5ad
QuanProCt1604 <127952803+QuanProCt1604@users.noreply.github.com>
2023-03-30 21:46:59 +0700

    Add files via upload

--- NguyenHongQuan/Program.cs
+++ NguyenHongQuan/Program.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exam
+﻿namespace NguyenHongQuan
 {
-    public class Program
+    internal class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello, World!");
         }
     }
 }
-
